0a67e2b4c65940a24315fb95643cd6db31e279ef
slackerr <jashannpreet@outlook.com>
2024-08-16 22:05:06 +0530

    made changes to backend

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ morgan.token('body', (request) =>JSON.stringify(request.body))
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 app.use(cors())
-/*app.use(express.static("build"))*/
+app.use(express.static('dist'))
+
 
 
 let persons = [
